15012ad0d8665de3fa505d3aea925ea12e1463bd
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2023-05-18 14:27:27 +0300

    Update TransferDetailsRepo.java

--- Eval1/src/main/java/com/eval1/repositories/TransferDetailsRepo.java
+++ Eval1/src/main/java/com/eval1/repositories/TransferDetailsRepo.java
@@ -1,7 +1,7 @@
 package com.eval1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.eval1.models.TransferDetails;
+import com.eval1.models.transfer.TransferDetails;
 
 public interface TransferDetailsRepo extends JpaRepository<TransferDetails, Long> {
 }
