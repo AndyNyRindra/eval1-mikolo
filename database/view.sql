717883fddaea1df7b08a362ca80938a74f93bce2
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2023-05-18 16:36:42 +0300

    Update view.sql

--- database/view.sql
+++ database/view.sql
@@ -13,9 +13,21 @@ group by shop_id, laptop_id;
 
 create or replace view  v_global_sales as
 select
+    row_number() over () as id,
     extract(month from date) mois,
     extract(year from date) annee,
     sum(amount) recettes,
     count(*) nombre_ventes
 from sale
 group by mois,annee;
+
+create or replace view  v_shop_sales as
+select
+    row_number() over () as id,
+    extract(month from date) mois,
+    extract(year from date) annee,
+    sum(amount) recettes,
+    count(*) nombre_ventes,
+    shop_id
+from sale
+group by mois,annee, shop_id;
